186fcea0e36ca14568dc7dbecd0588e0d3e4ac57
olive <bon_olives@yahoo.com>
2023-06-28 00:57:03 +0300

    END OF ASSIGNMENT

--- test.py
+++ test.py
@@ -6,16 +6,22 @@ from fibonachi import fibonacci
 def test_one():
     assert fibonacci(3) == [0, 1, 1]
 
+
 # Adding tests to evolve the Algorithm
 def test_two():
-    assert fibonacci(5) == [0,1,1,2,3]
+    assert fibonacci(5) == [0, 1, 1, 2, 3]
+
 
 def test_three():
-    assert fibonacci(4) == [0,1,1,2]
+    assert fibonacci(4) == [0, 1, 1, 2]
+
 
 def test_four():
-    assert fibonacci(10) == [0,1,1,2,3,5,8,13,21,34]
+    assert fibonacci(10) == [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+
 
 def test_five():
-    assert fibonacci(2) == [0,1]
+    assert fibonacci(2) == [0, 1]
 
+
+# END OF ASSIGNMENT
