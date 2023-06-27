ac2ef3954de9ddea5662a8ff5b268695f1735798
olive <bon_olives@yahoo.com>
2023-06-28 00:55:58 +0300

    ADD TESTS 1,2,3,4 AND 5 PASSED

--- test.py
+++ test.py
@@ -5,3 +5,17 @@ from fibonachi import fibonacci
 # defining the test one function for Fibonacci Sequence
 def test_one():
     assert fibonacci(3) == [0, 1, 1]
+
+# Adding tests to evolve the Algorithm
+def test_two():
+    assert fibonacci(5) == [0,1,1,2,3]
+
+def test_three():
+    assert fibonacci(4) == [0,1,1,2]
+
+def test_four():
+    assert fibonacci(10) == [0,1,1,2,3,5,8,13,21,34]
+
+def test_five():
+    assert fibonacci(2) == [0,1]
+
