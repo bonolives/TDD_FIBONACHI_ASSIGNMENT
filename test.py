3c64bea630e61af7bed3d62e9a150ddda9f39094
olive <bon_olives@yahoo.com>
2023-06-28 00:18:24 +0300

    ADD TEST ONE FAILED

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
-# Fibonachi assifnment : Richard Kwizera and Oliver Balyama
+# Fibonacci assignment : Richard Kwizera and Oliver Balyama
+
 
 # defining the test one function for Fibonacci Sequence
-def test():
-    assert fibonacci(3) == [0,1,1]
+def test_one():
+    assert fibonacci(3) == [0, 1, 1]
